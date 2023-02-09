6a3f4adf56934a29bc46327c9e04d2987fc7b0f7
Akash <phalke.aka@gmail.com>
2023-02-09 19:59:18 +0530

    update

--- 2static-website.tf
+++ 2static-website.tf
@@ -1,30 +1,30 @@
-resource "aws_s3_bucket" "class-s3-bucket" {
-    bucket = "class-bucket-922023"
-    acl = "public-read"
+# resource "aws_s3_bucket" "class-s3-bucket" {
+#     bucket = "class-bucket-922023"
+#     acl = "public-read"
 
-    website {
-        index_document = "index.html"
-        error_document = "error.html"
-    }
-    tags = {
-        Env = "DEV"
-    }
-    policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-      {
-          "Sid": "PublicReadGetObject",
-          "Effect": "Allow",
-          "Principal": "*",
-          "Action": [
-              "s3:GetObject"
-          ],
-          "Resource": [
-              "arn:aws:s3:::class-bucket-922023/*"
-          ]
-      }
-  ]
-}  
-EOF
-}
+#     website {
+#         index_document = "index.html"
+#         error_document = "error.html"
+#     }
+#     tags = {
+#         Env = "DEV"
+#     }
+#     policy = <<EOF
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#       {
+#           "Sid": "PublicReadGetObject",
+#           "Effect": "Allow",
+#           "Principal": "*",
+#           "Action": [
+#               "s3:GetObject"
+#           ],
+#           "Resource": [
+#               "arn:aws:s3:::class-bucket-922023/*"
+#           ]
+#       }
+#   ]
+# }  
+# EOF
+# }
