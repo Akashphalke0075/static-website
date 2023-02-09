edb297a0a3dcdf7f37e35ae32908e8ddc45be983
Akash <phalke.aka@gmail.com>
2023-02-09 19:36:34 +0530

    update

--- website-static.tf
+++ website-static.tf
@@ -1,29 +1,29 @@
-resource "aws_s3_bucket" "class-web-static-bucket" {
-  bucket = "class-web-bucket-02022023"
-  acl    = "public-read"
-  website {
-    index_document = "index.html"
-    error_document = "error.html"
-  }
-  tags = {
-    Env = "dev"
-  }
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-      {
-          "Sid": "PublicReadGetObject",
-          "Effect": "Allow",
-          "Principal": "*",
-          "Action": [
-              "s3:GetObject"
-          ],
-          "Resource": [
-              "arn:aws:s3:::class-web-bucket-02022023/*"
-          ]
-      }
-  ]
-}  
-EOF
-}
+# resource "aws_s3_bucket" "class-web-static-bucket" {
+#   bucket = "class-web-bucket-02022023"
+#   acl    = "public-read"
+#   website {
+#     index_document = "index.html"
+#     error_document = "error.html"
+#   }
+#   tags = {
+#     Env = "dev"
+#   }
+#   policy = <<EOF
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#       {
+#           "Sid": "PublicReadGetObject",
+#           "Effect": "Allow",
+#           "Principal": "*",
+#           "Action": [
+#               "s3:GetObject"
+#           ],
+#           "Resource": [
+#               "arn:aws:s3:::class-web-bucket-02022023/*"
+#           ]
+#       }
+#   ]
+# }  
+# EOF
+# }
