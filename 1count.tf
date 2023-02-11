0b643a54625fe07bc0ebce3c8b1b2735ebeb7dac
Akash <phalke.aka@gmail.com>
2023-02-11 18:55:45 +0530

    update

--- 1count.tf
+++ 1count.tf
@@ -1,9 +1,32 @@
-variable "user-list" {
-    default = ["Vikram","Akash","Sagar"]
+# variable "user-list" {
+#     default = ["Vikram","Akash","Sagar"]
 
+# }
+
+# resource "aws_iam_user" "new-user" {
+#     count = length(var.user-list)
+#     name = var.user-list[count.index]
+# }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+variable "create-user" {
+    default = ["prasad","ritesh","vikas"]
 }
 
-resource "aws_iam_user" "new-user" {
-    count = length(var.user-list)
-    name = var.user-list[count.index]
+resource "aws_iam_user" "new-user-iam" {
+    count = length(var.create-user)
+    name = var.create-user[count.index]
 }
