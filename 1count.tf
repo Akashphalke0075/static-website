7f6ce694e1e638789532b9df3a66d195b64ed1e8
Akash <phalke.aka@gmail.com>
2023-02-11 18:49:09 +0530

    update

---
+++ 1count.tf
@@ -0,0 +1,9 @@
+variable "user-list" {
+    default = ("Vikram","Akash","Sagar")
+
+}
+
+resource "aws_iam_user" "new-user" {
+    count = length(var.user-list)
+    name = var.user-list[count.index]
+}
