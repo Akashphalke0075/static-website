67e560218589eda4fb2b6432c7e0cf21368cd8ed
Akash <phalke.aka@gmail.com>
2023-02-12 01:07:49 +0530

    update

--- for-each.tf
+++ for-each.tf
@@ -2,7 +2,7 @@ variable "new-user" {
     default = ["akash", "vikas", "ninad"]
 }
 
-resource "aws_instance" "instance-create" {
+resource "aws_iam_user" "instance-create" {
     instance_type = "t2.micro"
     ami = "ami-0b5eea76982371e91"
     count = length(var.new-user)
