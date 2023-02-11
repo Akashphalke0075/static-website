c49f668043928a1e3024aa0653ba5085611799e7
Akash <phalke.aka@gmail.com>
2023-02-11 23:37:12 +0530

    update

--- var-count.tf
+++ var-count.tf
@@ -2,7 +2,7 @@ variable "user-list" {
     default = ["AKASH","VIAKS","VINOD"]
 }
 
-resource "aws_iam-user" "user-iam" {
-count = length(var.user-list)
-name = var.user-list[count.index]
+resource "aws_iam_user" "class-user" {
+    count = length(var.user-list)
+    name = var.user-list[count.index]
 }
