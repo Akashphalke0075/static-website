5bbf9222dc09d1ddc0cd43de40e48d9bfd647d78
Akash <phalke.aka@gmail.com>
2023-02-12 00:57:56 +0530

    update

--- for-each.tf
+++ for-each.tf
@@ -2,7 +2,7 @@ variable "user-list" {
     default = ["akash","manav","kunal","vikram",]
 }
 
-resource "user-list" "name-update" {
+resource "aws_iam_user" "name-update" {
     for_each = toset(var.user-list)
     name = each.key
 }
