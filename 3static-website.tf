464bda014727ed85b3976bcbfb828a5355bfacd0
Akash <phalke.aka@gmail.com>
2023-02-11 16:37:19 +0530

    update

--- 3static-website.tf
+++ 3static-website.tf
@@ -1,30 +1,30 @@
-resource "aws_s3_bucket" "new-bucket" {
-    bucket = "class-bucket-02092023"
-    acl = "public-read"
+# resource "aws_s3_bucket" "new-bucket" {
+#     bucket = "class-bucket-02092023"
+#     acl = "public-read"
 
-    website {
-        index_document = "index.html"
-        error_document = "error.html"
-    }
-    tags = {
-        Env = "Dev"
-    }
-policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-      {
-          "Sid": "PublicReadGetObject",
-          "Effect": "Allow",
-          "Principal": "*",
-          "Action": [
-              "s3:GetObject"
-          ],
-          "Resource": [
-              "arn:aws:s3:::class-bucket-02092023/*"
-          ]
-      }
-  ]
-}  
-EOF
-}
+#     website {
+#         index_document = "index.html"
+#         error_document = "error.html"
+#     }
+#     tags = {
+#         Env = "Dev"
+#     }
+# policy = <<EOF
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#       {
+#           "Sid": "PublicReadGetObject",
+#           "Effect": "Allow",
+#           "Principal": "*",
+#           "Action": [
+#               "s3:GetObject"
+#           ],
+#           "Resource": [
+#               "arn:aws:s3:::class-bucket-02092023/*"
+#           ]
+#       }
+#   ]
+# }  
+# EOF
+# }
---
+++ count.tf
@@ -0,0 +1,8 @@
+variable "user-list" {
+    default = ["akash","vikas","ninad"]
+}
+
+resource "aws_iam_user" "pr-create-usr" {
+    count = length[var.user-list]
+    name = var.user-list[count.index]
+}
