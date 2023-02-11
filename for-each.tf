8d809e75ee850302897b7f060a8b361745f3cd68
Akash <phalke.aka@gmail.com>
2023-02-12 01:08:29 +0530

    update

--- for-each.tf
+++ for-each.tf
@@ -3,8 +3,6 @@ variable "new-user" {
 }
 
 resource "aws_iam_user" "instance-create" {
-    instance_type = "t2.micro"
-    ami = "ami-0b5eea76982371e91"
     count = length(var.new-user)
     name = var.new-user[count.index]
 }
