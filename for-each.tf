7457fd91c5b4257a8ac7496351336b63b5193f7c
Akash <phalke.aka@gmail.com>
2023-02-14 19:20:46 +0530

    update

--- for-each.tf
+++ for-each.tf
@@ -1,8 +1,8 @@
-variable "user-list" {
-    default = ["akash", "shubham","kunal", "vipul", "vishal", "varun"]
-}
+# variable "user-list" {
+#     default = ["akash", "shubham","kunal", "vipul", "vishal", "varun"]
+# }
 
-resource "aws_iam_user" "user-iam" {
-    for_each = toset(var.user-list)
-    name = each.key
-}
+# resource "aws_iam_user" "user-iam" {
+#     for_each = toset(var.user-list)
+#     name = each.key
+# }
