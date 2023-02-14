653bd3845828f45158812b551227142d19f172c2
Akash <phalke.aka@gmail.com>
2023-02-14 20:55:15 +0530

    update

--- count.tf
+++ count.tf
@@ -1,10 +1,10 @@
-resource "aws_instance" "class-ec2" {
-    ami = "ami-0b5eea76982371e91"
-    instance_type = "t2.micro"
-    count = 3
+# resource "aws_instance" "class-ec2" {
+#     ami = "ami-0b5eea76982371e91"
+#     instance_type = "t2.micro"
+#     count = 3
 
-    tags = {
-        Name = "new-ec2-${count.index}"
-    }
-}
+#     tags = {
+#         Name = "new-ec2-${count.index}"
+#     }
+# }
 
--- for-each.tf
+++ for-each.tf
@@ -6,3 +6,14 @@
 #     for_each = toset(var.user-list)
 #     name = each.key
 # }
+
+
+variable "user-list" {
+    default = ["akash", "vinod",]
+
+}
+
+resource "aws_iam_user" "class-user" {
+    count = length(var.user-list)
+    name = var.user-list[count.index]
+}
