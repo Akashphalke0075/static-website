e8a6cfd8494f909bbcf67b7ce8e5c4e075fb082c
Akash <phalke.aka@gmail.com>
2023-02-14 18:31:03 +0530

    update

--- map-foreach.tf
+++ map-foreach.tf
@@ -2,7 +2,7 @@ resource "aws_s3_bucket" "class-bucket" {
     for_each = {
         dev = "pr-aws-bucket-14022023"
         test    = "pr-aws-bucket-14022023"
-        prod = "pr-aws-bucket-14022023"
+        prod = "pr-aws-bucket-140224023"
     }
    bucket = "${each.key}-${each.value}"
    acl = "private"
