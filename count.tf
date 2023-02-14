8b282a212135bacd17d23941bf2489c11ec55059
Akash <phalke.aka@gmail.com>
2023-02-14 18:59:57 +0530

    update

--- count.tf
+++ count.tf
@@ -6,3 +6,15 @@
 #     count = length(var.user-list)
 #     name = var.user-list[count.index]
 # }
+
+
+
+
+variable "user-list" {
+    default = ["akash", "vikas", "manoj"]
+}
+
+resource "aws_iam_user" "pr-user" {
+    count = length(var.user-list)
+    name = var.userlist[count.index]
+}
--- map-foreach.tf
+++ map-foreach.tf
@@ -1,13 +1,13 @@
-resource "aws_s3_bucket" "class-bucket" {
-    for_each = {
-        dev = "aws-bucket-14022023"
-        prod = "aws-bucket-14022023"
-        test = "aws-bucket-14022023"
-    }
-    bucket = "${each.key}-${each.value}"
-    acl = "private"
-    tags = {
-        enviornment = each.key
-        buckettnamee = "${each.key}-${each.value}"
-    }
-}
+# resource "aws_s3_bucket" "class-bucket" {
+#     for_each = {
+#         dev = "aws-bucket-14022023"
+#         prod = "aws-bucket-14022023"
+#         test = "aws-bucket-14022023"
+#     }
+#     bucket = "${each.key}-${each.value}"
+#     acl = "private"
+#     tags = {
+#         enviornment = each.key
+#         buckettnamee = "${each.key}-${each.value}"
+#     }
+# }
