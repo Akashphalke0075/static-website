01555cdf0918e360ba39827e1a2296f06e2f6dae
Akash <phalke.aka@gmail.com>
2023-02-15 20:44:36 +0530

    update

--- instance.tf
+++ instance.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "class-ec2-instance" {
     }
 }
 
-resource "aws_instance" "class-ec2-instance" {
+resource "aws_instance" "class-ec2-instance2" {
     ami = "ami-0b5eea76982371e91"
     instance_type = "t2.micro"
    provideer = aws.eu-west-1
