513dabad1a8b1c247ad86f8415b36875e9672193
Akash <phalke.aka@gmail.com>
2023-02-15 14:54:12 +0530

    update

--- 1version.tf
+++ 1version.tf
@@ -14,9 +14,23 @@
 
 
 
+# terraform {
+#     required_version = ">=1.1.0"
+#      required_providers  {
+#         aws = {
+#             source = "hashicorp/aws"
+#             version = "4.16"
+#         }
+#     }
+# }
+
+# provider "aws" {
+#     region = "us-east-1"
+# }
+
 terraform {
     required_version = ">=1.1.0"
-     required_providers  {
+    required_providers {
         aws = {
             source = "hashicorp/aws"
             version = "4.16"
--- 3static-website.tf
+++ 3static-website.tf
@@ -1,14 +1,45 @@
-resource "aws_s3_bucket" "new-bucket" {
-    bucket = "class-bucket-02092023"
-    acl = "public-read"
+# resource "aws_s3_bucket" "new-bucket" {
+#     bucket = "class-bucket-02092023"
+#     acl = "public-read"
 
-    website {
+#     website {
+#         index_document = "index.html"
+#         error_document = "error.html"
+#     }
+#     tags = {
+#         Env = "Dev"
+#     }
+# policy = <<EOF
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#       {
+#           "Sid": "PublicReadGetObject",
+#           "Effect": "Allow",
+#           "Principal": "*",
+#           "Action": [
+#               "s3:GetObject"
+#           ],
+#           "Resource": [
+#               "arn:aws:s3:::class-bucket-02092023/*"
+#           ]
+#       }
+#   ]
+# }  
+# EOF
+# }
+
+resource "aws_s3_bucket" "pr-request" {
+    bucket = "aws-s3-1520022023"
+    acl = "private"
+
+    wesbite {
         index_document = "index.html"
         error_document = "error.html"
     }
-    tags = {
-        Env = "Dev"
-    }
+  tags = {
+    env = "Dev"
+  }
 policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -21,7 +52,7 @@ policy = <<EOF
               "s3:GetObject"
           ],
           "Resource": [
-              "arn:aws:s3:::class-bucket-02092023/*"
+              "arn:aws:s3:::aws-s3-1520022023/*"
           ]
       }
   ]
