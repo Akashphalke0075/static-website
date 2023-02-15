ecc6d5dbbbcfacf0d2ea262c48e2073f1ad322e3
Akash <phalke.aka@gmail.com>
2023-02-15 14:37:09 +0530

    update

--- 1version.tf
+++ 1version.tf
@@ -16,8 +16,7 @@
 
 terraform {
     required_version = ">=1.1.0"
-    
-    required_providers = {
+     required_providers  {
         aws = {
             source = "hashicorp/aws"
             version = "4.16"
