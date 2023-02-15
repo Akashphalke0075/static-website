66ea56d90c98d3bae30d2acccfaef05650878825
Akash <phalke.aka@gmail.com>
2023-02-15 16:19:18 +0530

    update

--- for-each.tf
+++ for-each.tf
@@ -1,28 +1,28 @@
+# # # variable "user-list" {
+# # #     default = ["akash", "shubham","kunal", "vipul", "vishal", "varun"]
+# # # }
+
+# # # resource "aws_iam_user" "user-iam" {
+# # #     for_each = toset(var.user-list)
+# # #     name = each.key
+# # # }
+
+
 # # variable "user-list" {
-# #     default = ["akash", "shubham","kunal", "vipul", "vishal", "varun"]
+# #     default = ["akash", "vinod",]
+
 # # }
 
-# # resource "aws_iam_user" "user-iam" {
-# #     for_each = toset(var.user-list)
-# #     name = each.key
+# # resource "aws_iam_user" "class-user" {
+# #     count = length(var.user-list)
+# #     name = var.user-list[count.index]
 # # }
-
 
 # variable "user-list" {
-#     default = ["akash", "vinod",]
-
+#     default = ["akash", "vipyl", "ninad", "kartik", "varun"]
 # }
 
-# resource "aws_iam_user" "class-user" {
-#     count = length(var.user-list)
-#     name = var.user-list[count.index]
+# resource "aws_iam_user" "pr-user" {
+#     for_each = toset(var.user-list)
+#     name = each.key
 # }
-
-variable "user-list" {
-    default = ["akash", "vipyl", "ninad", "kartik", "varun"]
-}
-
-resource "aws_iam_user" "pr-user" {
-    for_each = toset(var.user-list)
-    name = each.key
-}
--- map-foreach.tf
+++ map-foreach.tf
@@ -11,3 +11,17 @@
 #         buckettnamee = "${each.key}-${each.value}"
 #     }
 # }
+
+resource "aws_s3_bucket" "class-bucket" {
+    for_each = {
+        dev = "aws-bucket-2023"
+        prod = "aws-bucket-2023"
+        test = "aws-bucket-2023"
+    }
+    bucket = "${each.key}-${each.value}"
+    acl = "private"
+
+    tags = {
+        env = "${each.key}-${each.value}"
+    }
+}
