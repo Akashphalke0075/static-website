93b0d5236400dc9b5a888e34886a2645aa3207b6
Akash <phalke.aka@gmail.com>
2023-02-15 20:45:22 +0530

    update

--- instance.tf
+++ instance.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "class-ec2-instance" {
 resource "aws_instance" "class-ec2-instance2" {
     ami = "ami-0b5eea76982371e91"
     instance_type = "t2.micro"
-   provideer = aws.eu-west-1
+   provider = aws.eu-west-1
     tags = {
         Name = "eu-west-demo"
     }
