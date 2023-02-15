cdf4b3f26e5463f9f91b23f4fbc046c72845c986
Akash <phalke.aka@gmail.com>
2023-02-15 21:07:03 +0530

    update

--- instance.tf
+++ instance.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "class-ec2-instance" {
 }
 
 resource "aws_instance" "class-ec2-instance2" {
-    ami = "ami-0b5eea76982371e91"
+    ami = "ami-0a5d9cd4e632d99c1"
     instance_type = "t2.micro"
    provider = aws.aws-eu-region
     tags = {
