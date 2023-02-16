aea8f15e68f8764eef4e5f939600babf595bf58a
Akash <phalke.aka@gmail.com>
2023-02-16 18:11:20 +0530

    update

--- lifecycle-demo.tf
+++ lifecycle-demo.tf
@@ -1,25 +1,36 @@
-resource "aws_instance" "class-ec2-instance" {
+# resource "aws_instance" "class-ec2-instance" {
+#     ami = "ami-0b5eea76982371e91"
+#     instance_type = "t2.micro"
+#     availability_zone = "us-east-1b"
+#     tags = {
+#         Name = "lifecycle-instance-new"
+#     }
+
+#     # lifecycle {
+#     #     prevent_destroy = true
+#     # }
+# #   lifecycle {
+# #     create_before_destroy = true
+# #   }
+
+# # lifecycle {
+# #     ignore_changes = [
+# #         tags
+# #     ]
+# # }
+
+# # lifecycle {
+# #     prevent_destroy = true
+# # }
+# }
+
+
+resource "aws_instance" "class-ec2" {
     ami = "ami-0b5eea76982371e91"
     instance_type = "t2.micro"
-    availability_zone = "us-east-1b"
+    availability_zone = "us-east-1a"
+
     tags = {
-        Name = "lifecycle-instance-new"
+        Name = "dev-dept"
     }
-
-    # lifecycle {
-    #     prevent_destroy = true
-    # }
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-
-# lifecycle {
-#     ignore_changes = [
-#         tags
-#     ]
-# }
-
-# lifecycle {
-#     prevent_destroy = true
-# }
 }
