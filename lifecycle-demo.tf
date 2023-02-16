c8bdfefa417e6c1d9fa53d50988331ef8c738962
Akash <phalke.aka@gmail.com>
2023-02-16 15:33:28 +0530

    update

--- lifecycle-demo.tf
+++ lifecycle-demo.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "class-ec2-instance" {
     ami = "ami-0b5eea76982371e91"
     instance_type = "t2.micro"
-
+    availability_zone = "us-east-1a"
     tags = {
-        Name = "us-east-demo"
+        Name = "lifecycle-instance-new"
     }
 
 }
