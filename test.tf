6972ae18c305f086ac3922c4e54d59861b994e11
Akash <phalke.aka@gmail.com>
2023-02-17 14:31:13 +0530

    update

--- test.tf
+++ test.tf
@@ -1,8 +1,8 @@
-variable “user_names” {
+variable "user_names" {
 default = ["Peter", "chris","stewie"]
 }
 
-resource “aws_iam_user” “quiz_experts_user” {
+resource "aws_iam_user" "quiz_experts_user" {
 count = length(var.user_names)
 name = var.user_names[count.index]
 }
